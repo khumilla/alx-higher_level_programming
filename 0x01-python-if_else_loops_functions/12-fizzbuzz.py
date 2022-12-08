06c08034e4fe99126b2b01615fd71d1c0a2dc392
khumilla <hold.the.dianhua@gmail.com>
2022-12-08 00:13:12 +0000

    correction1

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -13,6 +13,6 @@ def fizzbuzz():
         elif number % 3 == 0:
             print("Fizz ", end='')
         elif number % 5 == 0:
-            print("Buzz ", end=''
+            print("Buzz ", end='')
         else:
-        print("{}{}".format(number), end='')
+            print("{} ".format(number), end='')
