4db2a8ec292ed230df593c38d279d0d466ecf2a9
khumilla <hold.the.dianhua@gmail.com>
2022-12-21 00:51:02 +0000

    test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """represent a square."""
+
+    def __init__(self, size):
+        """Create a new square.
+
+        Arguments:
+            size (int): The size of the new square.priv instance attr.
+        """
+        self.__size = size
